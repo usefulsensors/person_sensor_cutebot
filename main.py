a0c2519dc6e2d35c367be67d884046127451c7ff
Pete Warden <pete@petewarden.com>
2022-11-27 17:35:27 -0800

    Implemented robot movement control

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # MicroPython example of reading the Person Sensor from Useful Sensors on a
-# Micro:bit. See https://usfl.ink/ps_dev for full documentation on the module,
-# and README.md in this project for details on wiring and assembly.
+# Micro:bit and using it to control a ring:bit robot car. See
+# https://usfl.ink/ps_dev for full documentation on the module, and README.md in
+# this project for details on wiring and assembly.
 
 from microbit import *
+
+from Ringbit import *
 
 import struct
 import time
@@ -27,6 +30,16 @@ PERSON_SENSOR_RESULT_BYTE_COUNT = struct.calcsize(PERSON_SENSOR_RESULT_FORMAT)
 
 # How long to pause between sensor polls.
 PERSON_SENSOR_DELAY = 0.2
+
+# Constants to control the movement behavior.
+TURN_THRESHOLD = 10
+TURN_SPEED = 20
+MIN_FACE_WIDTH = 30
+MAX_FACE_WIDTH = 35
+MOVE_SPEED = 40
+
+# Set up the ring:bit controller.
+RB = RINGBIT(pin1, pin2)
 
 # Keep looping and reading the person sensor results.
 while True:
@@ -59,6 +72,36 @@ while True:
         }
         faces.append(face)
     checksum = struct.unpack_from("H", read_data, offset)
-    print(num_faces, faces)
 
+    # If we've found any faces, the largest should be the first in the list, so
+    # use that to turn our robot to point at it.
+    left_speed = 0
+    right_speed = 0
+    if num_faces > 0:
+        main_face = faces[0]
+        face_center_x = (main_face["box_left"] + main_face["box_right"]) / 2
+        face_width = main_face["box_right"] - main_face["box_left"]
+        # Decide if we need to turn based on the position of the face.
+        turn_direction = (face_center_x - 128)
+        if turn_direction < -TURN_THRESHOLD:
+            turn_speed = -TURN_SPEED
+        elif turn_direction > TURN_THRESHOLD:
+            turn_speed = TURN_SPEED
+        else:
+            turn_speed = 0
+        left_speed = turn_speed
+        right_speed = -turn_speed
+        # If the face is too big, move away, or too small, move forward.
+        if face_width < MIN_FACE_WIDTH:
+            left_speed += MOVE_SPEED
+            right_speed += MOVE_SPEED
+        elif face_width > MAX_FACE_WIDTH:
+            left_speed -= MOVE_SPEED
+            right_speed -= MOVE_SPEED
+    if left_speed != 0 and right_speed != 0:
+        left_speed = min(100, max(-100, left_speed))
+        right_speed = min(100, max(-100, right_speed))
+        RB.set_motors_speed(left_speed, right_speed)
+        time.sleep(0.2)
+        RB.set_motors_speed(0, 0)
     time.sleep(PERSON_SENSOR_DELAY)
